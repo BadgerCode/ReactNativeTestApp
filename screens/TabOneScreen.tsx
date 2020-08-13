e872b4d8bfecbee717e80f73dcbd62dcb600e57c
Michael Hawkins <BadgerCode@users.noreply.github.com>
2020-08-13 21:41:51 +0100

    Messes around with some files

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image, ScrollView } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Tab One 👌</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+        <Text>Hello</Text>
+        <Image
+          source={{ uri: "https://reactnative.dev/docs/assets/p_cat1.png" }}
+          style={{ width: 200, height: 200 }}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
